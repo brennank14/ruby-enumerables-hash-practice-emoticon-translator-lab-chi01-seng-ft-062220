6bce2d2b97d4a78ff9e65fddf4d52d6a63d1f645
Kathleen Brennan <brennank14@gmail.com>
2020-06-12 02:00:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ end
 
 def get_japanese_emoticon (translations, emoticon)
   library = load_library(translations)
-  library.find do |key, value|
+  library.keys.find do |key, value|
     binding.pry
     if emoticon == value[:english]
       #binding.pry
