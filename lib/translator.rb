dd7a79988b90b248e2203d409331c9067574b6fa
Kathleen Brennan <brennank14@gmail.com>
2020-06-12 02:12:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,25 +18,25 @@ end
 def get_japanese_emoticon (translations, emoticon)
   library = load_library(translations)
   found_emoticon = library.keys.find do |key|
-    #binding.pry
-    emoticon == library[key][:english]
+  emoticon == library[key][:english]
   end
 
-    if found_emoticon
-      #binding.pry
-      library[found_emoticon][:japanese]
-    else
+  if found_emoticon
+    library[found_emoticon][:japanese]
+  else
       "Sorry, that emoticon was not found"
-    end
+  end
 end
 
 
 def get_english_meaning(translations, emoticon)
-  load_library(translations) do |key, value|
-    if emoticon == new_hash[key][:japanese]
-      new_hash[key]
-    else
+  library = load_library(translations)
+  found_emoticon = library.keys.find do |key|
+  emoticon == library[key][:japanese]
+
+  if found_emoticon
+    library[found_emoticon]
+  else
       "Sorry, that emoticon was not found"
-    end
   end
 end
